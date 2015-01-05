5ec5f549bc74fe8d4a302442c5293e05e1061112
Rasmus Fuhse <krassmus@gmail.com>
2015-01-05 14:15:23 +0100

    try to disable big image handler for icons

--- views/presenting/users_plugins.php
+++ views/presenting/users_plugins.php
@@ -11,15 +11,15 @@
                     <? $score = $marketplugin->getRating() ?>
                     <? $score = round($score, 1) / 2 ?>
                     <? $v = $score >= 1 ? 3 : ($score >= 0.5 ? 2 : "") ?>
-                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px")) ?>
+                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px", 'class' => "big-image-handled")) ?>
                     <? $v = $score >= 2 ? 3 : ($score >= 1.5 ? 2 : "") ?>
-                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px")) ?>
+                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px", 'class' => "big-image-handled")) ?>
                     <? $v = $score >= 3 ? 3 : ($score >= 2.5 ? 2 : "") ?>
-                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px")) ?>
+                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px", 'class' => "big-image-handled")) ?>
                     <? $v = $score >= 4 ? 3 : ($score >= 3.5 ? 2 : "") ?>
-                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px")) ?>
+                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px", 'class' => "big-image-handled")) ?>
                     <? $v = $score > 4.5 ? 3 : ($score >= 4.5 ? 2 : "") ?>
-                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px")) ?>
+                    <?= Assets::img($plugin->getPluginURL()."/assets/star$v.svg", array('width' => "16px", 'class' => "big-image-handled")) ?>
                 </span>
             </td>
             <td>
