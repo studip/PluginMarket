83cce92be23b3bb560d618766033011f2e33f9e5
Rasmus Fuhse <fuhse@data-quest.de>
2015-03-19 15:07:35 +0100

    fixes #9

--- views/extern/xml.php
+++ views/extern/xml.php
@@ -12,7 +12,7 @@
             version="<?= htmlReady(studip_utf8encode($release['version'])) ?>"
             studipMinVersion="<?= htmlReady(studip_utf8encode($release['studip_min_version'])) ?>"
             studipMaxVersion="<?= htmlReady(studip_utf8encode($release['studip_min_version'])) ?>"
-            url="<?= htmlReady(studip_utf8encode($GLOBALS['ABSOLUTE_URI_STUDIP'].$controller->url_for('presenting/download/' . $release->getId()))) ?>"
+            url="<?= htmlReady(studip_utf8encode($GLOBALS['ABSOLUTE_URI_STUDIP']."plugins.php/pluginmarket/presenting/download/" . $release->getId())) ?>"
             />
         <? endforeach ?>
     </plugin>
