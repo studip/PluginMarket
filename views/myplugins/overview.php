a78cfd0c18eb28386d02d560aeca15b8e6b07115
David Siegfried <support@ds-labs.de>
2016-11-21 09:08:29 +0100

    replace Asssets against Icon and fix errors

--- views/myplugins/overview.php
+++ views/myplugins/overview.php
@@ -8,48 +8,51 @@
         </tr>
     </thead>
     <tbody>
-<? if (count($plugins)):  ?>
-    <? foreach ($plugins as $marketplugin): ?>
-        <tr>
-            <td>
-            <? if ($marketplugin['publiclyvisible'] && !$marketplugin['approved']) : ?>
-                <?= Assets::img("icons/20/red/exclaim-circle", array('title' => _("Plugin wurde noch nicht von einem Administrator freigeschaltet."), 'class' => "text-bottom")) ?>
-            <? endif; ?>
-                <a href="<?= $controller->url_for('presenting/details/' . $marketplugin->getId()) ?>">
-                    <?= htmlReady($marketplugin['name']) ?>
-                </a>
-            </td>
-            <td>
-            <?
-                $chdate = $marketplugin['chdate'];
-                foreach ($marketplugin->releases as $release) {
-                    $chdate = max($chdate, $release['chdate']);
-                }
-            ?>
-                <?= date("j.n.Y, G:i", $chdate) ?> <?= _("Uhr") ?>
-            </td>
-            <td>
-            <? if (!$marketplugin['publiclyvisible']) :  ?>
-                <?= Assets::img("icons/20/grey/lock-locked.png.png", array('title' => _("Plugin ist nicht öffentlich"))) ?>
-            <? endif ?>
-            </td>
-            <td class="actions">
-                <a href="<?= $controller->url_for('myplugins/edit/' . $marketplugin->getId()) ?>" data-dialog title="<?= _("Plugin-Info bearbeiten") ?>">
-                    <?= Assets::img('icons/20/blue/edit') ?>
-                </a>
-                <a href="<?= $controller->url_for('myplugins/add_release/' . $marketplugin->getId()) ?>" data-dialog title="<?= _("Neues Release hinzufügen") ?>">
-                    <?= Assets::img("icons/20/blue/add") ?>
-                </a>
-            </td>
-        </tr>
-    <? endforeach; ?>
-<? else: ?>
-        <tr>
-            <td colspan="4" style="text-align: center;">
-                <?= _("Sie haben noch kein Plugin eingestellt.") ?>
-            </td>
-        </tr>
-<? endif; ?>
+        <? if (count($plugins)): ?>
+            <? foreach ($plugins as $marketplugin): ?>
+                <tr>
+                    <td>
+                        <? if ($marketplugin['publiclyvisible'] && !$marketplugin['approved']) : ?>
+                            <?= Icon::create('exclaim-circle', 'status-red', ['title' => _("Plugin wurde noch nicht von einem Administrator freigeschaltet."),
+                                                                              'class' => "text-bottom"]) ?>
+                        <? endif; ?>
+                        <a href="<?= $controller->url_for('presenting/details/' . $marketplugin->getId()) ?>">
+                            <?= htmlReady($marketplugin['name']) ?>
+                        </a>
+                    </td>
+                    <td>
+                        <?
+                        $chdate = $marketplugin['chdate'];
+                        foreach ($marketplugin->releases as $release) {
+                            $chdate = max($chdate, $release['chdate']);
+                        }
+                        ?>
+                        <?= date("j.n.Y, G:i", $chdate) ?> <?= _("Uhr") ?>
+                    </td>
+                    <td>
+                        <? if (!$marketplugin['publiclyvisible']) : ?>
+                            <?= Icon::create('lock-locked', 'inactive', ['title' => _("Plugin ist nicht öffentlich")]) ?>
+                        <? endif ?>
+                    </td>
+                    <td class="actions">
+                        <a href="<?= $controller->url_for('myplugins/edit/' . $marketplugin->getId()) ?>" data-dialog
+                            title="<?= _("Plugin-Info bearbeiten") ?>">
+                            <?= Icon::create('edit') ?>
+                        </a>
+                        <a href="<?= $controller->url_for('myplugins/add_release/' . $marketplugin->getId()) ?>"
+                            data-dialog title="<?= _("Neues Release hinzufügen") ?>">
+                            <?= Icon::create('add') ?>
+                        </a>
+                    </td>
+                </tr>
+            <? endforeach; ?>
+        <? else: ?>
+            <tr>
+                <td colspan="4" style="text-align: center;">
+                    <?= _("Sie haben noch kein Plugin eingestellt.") ?>
+                </td>
+            </tr>
+        <? endif; ?>
     </tbody>
 </table>
 
@@ -58,7 +61,7 @@ $sidebar = Sidebar::Get();
 $sidebar->setImage(Assets::image_path("sidebar/plugin-sidebar.png"));
 $actions = new ActionsWidget();
 $actions->addLink(_("Neues Plugin eintragen"),
-                  $controller->url_for('myplugins/add'),
-                  'icons/16/blue/add.png')->asDialog();
+    $controller->url_for('myplugins/add'),
+    Icon::create('add'))->asDialog();
 $sidebar->addWidget($actions);
 
