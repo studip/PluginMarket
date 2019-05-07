b4d138af8aae900df93b3b2044065a00cf6b4d75
Rasmus Fuhse <fuhse@data-quest.de>
2019-05-07 09:58:34 +0200

    add release

--- PluginMarket.class.php
+++ PluginMarket.class.php
@@ -13,7 +13,7 @@ class PluginMarket extends StudIPPlugin implements SystemPlugin, HomepagePlugin
             '2.0', '2.1', '2.2', '2.3', '2.4',
             '2.5',
             '3.0', '3.1', '3.2', "3.3", "3.4", "3.5",
-            "4.0", "4.1"
+            "4.0", "4.1", "4.2", "4.3"
         );
     }
 
